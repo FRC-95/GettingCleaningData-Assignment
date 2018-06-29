fcf225e39387a672e95f8d32d9202fc2e59e2460
FRC95 <Franruzafa95@gmail.com>
2018-06-29 14:07:17 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -13,21 +13,21 @@ activity.label <- read.table("activity_labels.txt", header = F)
 subject.train <- read.table("train/subject_train.txt", header = F)
 subject.test <- read.table("test/subject_test.txt", header = F)
 
-# Get feature labels
+# 2. Get feature labels
 features.names <- read.table("features.txt", header = F)
 features.mean.std <-grep(".*mean.*|.*std.*", features.names$V2)
 
-# Read only wanted features
+# 3. Read only wanted features
 features.train <- read.table("train/X_train.txt", header = F)[features.mean.std]
 features.test <- read.table("test/X_test.txt", header = F)[features.mean.std]
 
-## 2. Merge train and test
+## 4. Merge train and test
 
 activity.data <- rbind(activity.train, activity.test)
 features.data <- rbind(features.train, features.test)
 subject.data <- rbind(subject.train, subject.test)
 
-## 3. Set levels, labels and colnames
+## 5. Set levels, labels and colnames
 
 activity.data$V1 <- factor(activity.data$V1,
                            levels = activity.label$V1,
@@ -36,11 +36,12 @@ colnames(activity.data) <- "activity"
 colnames(subject.data) <- "subject"
 colnames(features.data) <- features.names$V2[features.mean.std]
 
-## 5. Merge in one data set
+## 6. Merge in one data set
 
 data <- cbind(subject.data, activity.data, features.data)
 
-## 6. Independent tidy data organised by means
+## 7. Independent tidy data organised by means
 
 data.means <- data %>% group_by(subject,activity) %>% summarize_all(mean)
+
 
